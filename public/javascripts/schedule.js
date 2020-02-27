e151118d4ad5dba8318fea418445d956c433ebdf
Juan Sebastián Bravo Castelo <js.bravo@uniandes.edu.co>
2020-02-27 13:28:19 -0500

    Feature Schedule

--- public/javascripts/schedule.js
+++ public/javascripts/schedule.js
@@ -1,63 +1,217 @@
-/* eslint-disable no-undef */
-document.addEventListener("DOMContentLoaded", function() {
+var calendar;
+
+document.addEventListener("DOMContentLoaded", function () {
     var calendarEl = document.getElementById("calendar");
     //dayGrid dayGridWeek dayGridMonth listWeek 
-    var view = "dayGrid";
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        plugins: [ "timeGrid","dayGrid", "list" ],
+    var view = "dayGridMonth";
+
+    calendar = new FullCalendar.Calendar(calendarEl, {
+        buttonText: {
+            today: "Hoy"
+        },
+        noEventsMessage: "No hay eventos este día",
+        eventLimit: true,
+        nowIndicator: true,
+        themeSystem: "bootstrap",
+        plugins: ["bootstrap", "timeGrid", "dayGrid", "list"],
+        bootstrapFontAwesome: true,
+
         customButtons: {
             day: {
                 text: "Dia",
-                click: function() {
+                click: function () {
                     view = "dayGrid";
                     calendar.changeView("dayGrid");
                 }
             },
             week: {
                 text: "Semana",
-                click: function() {
+                click: function () {
                     view = "dayGridWeek";
-                    calendar.changeView("dayGridWeek");
+                    calendar.changeView("timeGridWeek");
                 }
             },
             month: {
                 text: "Mes",
-                click: function() {
+                click: function () {
                     calendar.changeView("dayGridMonth");
                 }
             },
             list: {
                 text: "Lista",
-                click: function() {
+                click: function () {
                     calendar.changeView("listWeek");
                 }
             }
 
         },
         header: {
+            close: "fa-times",
+            prev: "fa-chevron-left",
+            next: "fa-chevron-right",
+            prevYear: "fa-angle-double-left",
+            nextYear: "fa-angle-double-right",
             left: "prev,next today",
             center: "title",
             right: "day,week,month,list"
         },
         defaultView: view,
-        locale: "es",
-        events: [
-            {
-                title: "Objeto 1",
-                start: "2020-02-22T14:30:00",
-                extendedProps: {
-                    status: "done"
-                }
+        locale: "es-ES",
+        eventClick: function(info) {
+            loadModal(info.event);
+            console.log(info.event);
+            console.log(info.event.extendedProps);
+        },        
+        events: [{
+            title: "Objeto 1",
+            start: "2020-02-22T14:30:00",
+            extendedProps: {
+                estado: "Arrendado",
+                arrendatario: "js.bravo@uniandes.edu.co",
+                arrendador: "Juan Sebastián Bravo",
+                id: 1
             },
-            {
-                title: "Calcuadora Texas",
-                start: "2020-02-22T07:00:00",
-                backgroundColor: "green",
-                borderColor: "green"
-            }
+        },
+        {
+            title: "Calcuadora Texas",
+            start: "2020-02-22T07:00:00",
+            backgroundColor: "green",
+            borderColor: "green"
+        }
         ],
     });
+
     calendar.render();
 });
 
-$("#buttonName").click(function () {  });
+const loadModal = (event) => 
+{
+    $(".modal-title")[0].innerHTML = event.title;
+    $("#arrendador").val(event.extendedProps.arrendador);
+    $("#descripcion").val(event.extendedProps.description);
+    $("#precioHora").val(event.extendedProps.precioHora);
+    $("#precioHora").val(event.extendedProps.precioDia);
+    if(event.extendedProps.arrendador)
+    {
+        $("#descripcion").attr("disabled", true);
+        $("#precioHora").attr("disabled", true);
+        $("#precioDia").attr("disabled", true);
+        $("#borrar").attr("disabled", true);
+        $("#guardar").attr("disabled", true);
+    }
+    else
+    {
+        $("#descripcion").removeAttr("disabled");
+        $("#precioHora").removeAttr("disabled");
+        $("#precioDia").removeAttr("disabled");
+        $("#borrar").removeAttr("disabled");
+        $("#guardar").removeAttr("disabled");
+    }
+    $(".modal").show();
+};
+
+const loadMyObject = (id) => 
+{
+    calendar.destroy();
+    //TODO: Fetch objeto por id
+    //let object = fetch
+    let object = {
+        id: 1,
+        name: "Calculadora Texas",
+        description: "Arriendo calculadora texas",
+        priceHour: "$2000/hora",
+        priceDay: "$5000/día",
+        arrendador: {
+            userName: "Juan Sebastián Bravo",
+            email: "js.bravo@uniandes.edu.co"},
+        usuariosInteresados: ["js.bravo@uniandes.edu.co"],
+        events:[{
+            start: new Date(),
+            end: new Date() +1000000,
+            /* https://fullcalendar.io/docs/recurring-events 
+        duration: */
+            title: "Calculadora",
+            state: "Arrendado",
+            usuarioArrendatario: "js.bravo@uniandes.edu.co",
+            esRecurrente: true,
+            daysOfWeek:[0,1,2,3,4],
+            startRecur: new Date(),
+            endRecur: new Date() + 200000,
+            /* hh:mm:sss */
+            //startTime: "09:20",
+            //endTime: "10:20",
+            allDay: true}]
+      
+      
+    };
+    var calendarEl = document.getElementById("calendar");
+    //dayGrid dayGridWeek dayGridMonth listWeek 
+    var view = "dayGridMonth";
+
+    calendar = new FullCalendar.Calendar(calendarEl, {
+        buttonText: {
+            today: "Hoy"
+        },
+        noEventsMessage: "No hay eventos este día",
+        eventLimit: true,
+        nowIndicator: true,
+        themeSystem: "bootstrap",
+        plugins: ["bootstrap", "timeGrid", "dayGrid", "list"],
+        bootstrapFontAwesome: true,
+
+        customButtons: {
+            day: {
+                text: "Dia",
+                click: function () {
+                    view = "dayGrid";
+                    calendar.changeView("dayGrid");
+                }
+            },
+            week: {
+                text: "Semana",
+                click: function () {
+                    view = "dayGridWeek";
+                    calendar.changeView("timeGridWeek");
+                }
+            },
+            month: {
+                text: "Mes",
+                click: function () {
+                    calendar.changeView("dayGridMonth");
+                }
+            },
+            list: {
+                text: "Lista",
+                click: function () {
+                    calendar.changeView("listWeek");
+                }
+            }
+
+        },
+        header: {
+            close: "fa-times",
+            prev: "fa-chevron-left",
+            next: "fa-chevron-right",
+            prevYear: "fa-angle-double-left",
+            nextYear: "fa-angle-double-right",
+            left: "prev,next today",
+            center: "title",
+            right: "day,week,month,list"
+        },
+        defaultView: view,
+        locale: "es-ES",
+        eventClick: function(info) {
+            loadModal(info.event);
+            console.log(info.event);
+            console.log(info.event.extendedProps);
+        },        
+        events: object.events,
+    });
+
+    calendar.render();
+};
+const closeModal = () => $(".modal").hide();
+
+$(".modal").on("shown.bs.modal", function () {
+    $("#myInput").trigger("focus");
+});
--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,8 @@ router.get("/", async function (req, res, next) {
     title: "Home",
     active: {
       home: true
-    }
+    },
+    home: true
   };
 
   const accessToken = await authHelper.getAccessToken(req.cookies, res);
@@ -29,8 +30,59 @@ router.get("/", async function (req, res, next) {
   res.render("index", parms);
 });
 
-router.get("/schedule", function(req, res) {
-  res.render("schedule");
+router.get("/schedule", async function(req, res) {
+  let parms = {
+    title: "Schedule",
+    active: {
+      home: true
+    },
+    sidebar: true
+  };
+
+  parms.myObjects = [{
+    id: 1,
+    name: "Calculadora Texas",
+    description: "Arriendo calculadora texas",
+    priceHour: "$2000/hora",
+    priceDay: "$5000/día",
+    arrendador: {
+      userName: "Juan Sebastián Bravo",
+      email: "js.bravo@uniandes.edu.co"},
+    usuariosInteresados: ["js.bravo@uniandes.edu.co"],
+    events:[{
+      start: new Date(),
+      end: new Date(),
+      /* https://fullcalendar.io/docs/recurring-events 
+    duration: */
+      title: "Calculadora",
+      state: "Arrendado",
+      usuarioArrendatario: "js.bravo@uniandes.edu.co",
+      esRecurrente: true,
+      daysOfWeek:[0,1,2,3,4],
+      startRecur: new Date(),
+      endRecur: new Date(),
+      /* hh:mm:sss */
+      startTime: "09:20",
+      endTime: "09:20"}]
+  
+  
+  }];
+  parms.rentedObjects = [{name:"No Texas"}, {name:"No Texas 2 "}, {name:"No Texas 3"}];
+
+  const accessToken = await authHelper.getAccessToken(req.cookies, res);
+  const userName = req.cookies.graph_user_name;
+  const userEmail = req.cookies.graph_user_email;
+
+  if (accessToken && userName) {
+    parms.accessToken = accessToken;
+    parms.userName = userName;
+    parms.userEmail = userEmail;
+    parms.userToken = accessToken;
+  } else {
+    parms.signInUrl = authHelper.getAuthUrl();
+    parms.debug = parms.signInUrl;
+  }
+  res.render("schedule",parms);
 });
 
 router.get("/rent", async function (req, res) { 
