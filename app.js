126507bcf3b2ad879e592787310bd8ad8dead4e1
Juan Sebastián Bravo Castelo <js.bravo@uniandes.edu.co>
2020-03-02 12:13:54 -0500

    Corrección .env

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var logger = require("morgan");
 require("dotenv").config();
 var indexRouter = require("./routes/index");
 var authorize = require("./routes/authorize");
-var usersRouter = require("./routes/users");
 var upload = require("express-fileupload");
 
 var app = express();
@@ -26,7 +25,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/authorize", authorize);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
